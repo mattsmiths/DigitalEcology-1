d1f3a1efdddbd812a0efabfc1fd726f77888a0fa
jamescrall <james.crall@gmail.com>
2022-11-04 20:43:02 -0500

    updating scripts

--- Feather/examples/NeoPixel_test.py
+++ Feather/examples/NeoPixel_test.py
@@ -8,15 +8,18 @@ import neopixel
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 pixel.brightness = 0
-br_itr = 0.1
-delay = 0.01
-col_iter = 2
+br_itr = 0.1 #Bright iterator - how long to delay 
+delay = 0.01 #Delay between updates to the color
+col_iter = 2 #interval to update the color channels (within range of 0 - 255)
+
+#Set initial values for colors
 rv = 255
 gv = 0
 bv = 255
 while True:
     while rv > 0:
-        rv = rv - col_iter
+    	#Step colors
+        rv = rv - col_iter 
         gv = gv + col_iter
         bv = bv - col_iter
         time.sleep(delay)
